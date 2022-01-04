162ef69f05989eb5d60ad960105807956add0b6e
slavablack <veaceslav.ciornii@magenta.de>
2022-01-04 13:23:20 +0100

    winners

---
+++ index.js
@@ -0,0 +1,8 @@
+const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+function finalWinners (nameWinners) {
+    winners.push(nameWinners)
+ console.log(winners)
+};
+console.log(winners)
+finalWinners("Rebeca");
+
