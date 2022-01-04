3d503eaf923d36f4e532d6db9dba241203238bd0
Dennisbullys <d.rochus@hotmail.de>
2022-01-04 11:22:30 +0100

    Dennis

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ winners.push("Dennis");
 winners.push("Alexandra");
 winners.push("Lucas");
 // Sortiert nach Alphabet ( Bonus )
-winners.sort()
-console.log(winners);
+winners.sort();
+console.log("Gewonnen haben :",winners);
+
 
