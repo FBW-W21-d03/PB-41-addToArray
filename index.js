f749dbb4de9e9567eea6c0e2425502d631896fc6
Mak513 <osei.makangu@hotmail.com>
2022-01-04 13:19:09 +0100

    osei

---
+++ index.js
@@ -0,0 +1,8 @@
+const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+function gewonnen(name) {
+  console.log("Dies sind die Gewinner", winners.push(name), winners);
+}
+
+gewonnen("Osei");
+gewonnen("Mak");
+gewonnen("Tim");
