fe6cad0d133c47251a52751499dd1182da2ecdcd
Dennisbullys <d.rochus@hotmail.de>
2022-01-04 11:11:23 +0100

    Dennis

---
+++ main.js
@@ -0,0 +1,12 @@
+// Aufgabe 1
+
+const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+
+
+winners.push("Dennis");
+winners.push("Alexandra");
+winners.push("Lucas");
+// Sortiert nach Alphabet ( Bonus )
+winners.sort()
+console.log(winners);
+
