bb86cb841283092362185d0b71872d80cf719b10
Hayat Alkheder <hayat.alkheder@gmail.com>
2022-01-04 15:54:35 +0100

    js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // 1)
-const winners =["Greg", "Flizzy", "Putput", "Hannibal"];
+const winners =["Greg", "Flizzy","Putput", "Hannibal"];
 
 // 2)
 function gewinner(name) {
    winners.push(name);
+   return winners;
 }
 
-gewinner("Sarah");
-gewinner("Hayat");
-gewinner("Rodi");
-console.log(winners);
+ let return1= gewinner("Sarah");
+ let return2= gewinner("Hayat");
+ let return3= gewinner("Rodi");
+console.log(return1,return2,return3);
 
     
