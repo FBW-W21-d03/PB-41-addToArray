ea3e5ae434d4cf49e5645c9e2a9fba8857869554
sprotte <metatron1972@live.de>
2022-01-04 15:59:03 +0100

    sprotte

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ winners.push("Nic");
 winners.push("Nac");
 winners.push("Noc");
 
-function gewiner(name) {
+function gewinner(name) {
   winners.push(name);
   return winners.length;
 }
 
 winners.sort();
+console.log(return1, return2, return3);
 console.log("And the Winner is :", winners);
 console.table(winners);
