949fee2733b56c554d248eba2bf7b2c226398189
Mago <89070052+ShiinyFlakes@users.noreply.github.com>
2022-01-04 15:58:17 +0100

    Update return

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const liste = ["Greg", "Flizzy", "Putin", "Hannibal"];
 function hinzufügen(name) {
     return liste.push(name)};
     hinzufügen("Mago") + liste.length;
-let return1= liste;
-let return2= liste.length;
+let return1= liste + liste.length;
 console.log(return1);
-console.log(return2);
 
 
