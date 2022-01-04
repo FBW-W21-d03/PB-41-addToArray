34c906fb5f51e26790d7ec309e7fb8c150be82a5
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2022-01-04 10:44:30 +0100

    übung addToArray

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("------------------------");
+console.log("javascript-addToArray");
+
+// aufgabe teil 1
+
+const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+
+console.log(winners);
+
+// aufgabe teil 2
+
+winners.push('Angelika');
+winners.push('Frank');
+console.log(winners)
