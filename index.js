e8dc305057e1e1e42ece0d2bd7adb30a5a307948
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-04 13:30:36 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,9 @@
+const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+
+function addNewWinners(name) {
+  winners.push(name);
+}
+
+addNewWinners("Rajwinder");
+addNewWinners("john");
+console.log(winners);
