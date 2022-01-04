4855e769eb2c6fbe3ed5e1c45270e090b4edfd1d
Vens <v.kolev@alfatrex.com>
2022-01-04 13:17:55 +0100

    vens lüsung

---
+++ script.js
@@ -0,0 +1,11 @@
+const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+
+console.log(winners);
+
+winners.push('Betty');
+winners.push('Max');
+winners.push('Axell');
+
+console.log(winners[2]);
+console.log(winners[3]);
+console.log(winners[6]);
