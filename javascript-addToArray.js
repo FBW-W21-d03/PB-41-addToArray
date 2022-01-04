6df7036fe47508beb1af9d73c5216d96f6994642
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-04 13:14:39 +0100

    Orhan's lösung

---
+++ javascript-addToArray.js
@@ -0,0 +1,11 @@
+// Aufgabe 1
+const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+
+// Aufgabe 2
+function gewiner(name) {
+
+    winners.push(name);
+}
+
+gewiner("Orhan");
+console.log(winners);
