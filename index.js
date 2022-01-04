4c93de6874633a605cd5ea23cb9c3a95f66e2709
kawanaza <kawanbabani@gmail.com>
2022-01-04 15:41:07 +0100

    Done mit return

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
+const winners = ["", "Greg", "Flizzy", "Putput", "Hannibal"];
 
 function gewiner(name) {
   winners.push(name);
+  return winners.length;
 }
 
-gewiner("kawan");
-gewiner("John");
+let winners2 = gewiner("kawan");
+
+console.log(winners2);
+gewiner("John Smith");
 gewiner("Keven");
+gewiner("Serdar");
+gewiner("Sprote");
 console.table(winners);
