22c5c14cb709a5976b83ee6d9ca0339f854c1104
Vens <v.kolev@alfatrex.com>
2022-01-04 15:32:35 +0100

    vens return

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];
 
 function addWinner(name){
     winners.push(name);
+    return name.length;
 }
 
 addWinner('Betty');
 addWinner('Max');
 addWinner('Axell');
 
-console.log(winners[2]);
-console.log(winners[3]);
-console.log(winners[6]);
+let returnet = addWinner('Betty');
+let returnet1 = addWinner('Max');
+let returnet2 = addWinner('Axell');
+
+console.log("Der Name ", winners[4], " ist folgende Zeichen lang: ", returnet);
+console.log("Der Name ", winners[5], " ist folgende Zeichen lang: ", returnet1);
+console.log("Der Name ", winners[6], " ist folgende Zeichen lang: ", returnet2);
