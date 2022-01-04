43527b0f99f2c5414cc519c278753cf88d7e2ce0
Mago <89070052+ShiinyFlakes@users.noreply.github.com>
2022-01-04 15:55:59 +0100

    Update return

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const winners = ["Greg", "Flizzy", "Putput", "Hannibal"]; 
-function gewinner(name) { 
-winners.push(name);} gewinner("Mago");
-console.log(winners);
+const liste = ["Greg", "Flizzy", "Putin", "Hannibal"];
+function hinzufügen(name) {
+    return liste.push(name)};
+    hinzufügen("Mago") + liste.length;
+let return1= liste;
+let return2= liste.length;
+console.log(return1);
+console.log(return2);
 
 
