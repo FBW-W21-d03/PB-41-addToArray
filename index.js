802e90825ee60cbcd3f39e2deec66bded219bd6a
Sharfuddin Ahmed <Sharfuddinahmed07@gmail.com>
2022-01-04 13:44:21 +0100

    Meine Lösung

---
+++ index.js
@@ -0,0 +1,19 @@
+/* Aufgabenstellung - Teil 1
+Schreibe das folgende Array in JavaScript: const winners = ["Greg", "Flizzy", "Putput", "Hannibal"];*/
+
+const winners = ["Greg","Flizzy","Putput","Hannibal"];
+
+
+
+/* Aufgabenstellung - Teil 2
+Schreibe eine Funktion, die einen Gewinner in das Array hinzufügt.
+Die Funktion nimmt den Namen des Gewinners über einen Parameter auf.
+Anschließend fügt die Funktion den Namen dem Array hinzu.*/
+
+function addWinners (name){ 
+winners.push(name);
+
+};
+
+addWinners("Jemes")
+console.log (winners)
